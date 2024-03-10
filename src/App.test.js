e538ead1481f59e4cec9869e8fbd780daf7b5cde
Pavan Kalyan <125635201+pkalyan607@users.noreply.github.com>
2024-03-10 20:44:00 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,12 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // Rendering the App component
   render(<App />);
+
+  // Finding an element with the text "learn react" case-insensitively
   const linkElement = screen.getByText(/learn react/i);
+
+  // Expecting the linkElement to be in the document
   expect(linkElement).toBeInTheDocument();
 });
