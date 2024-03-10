dfc76df4f27b4048399ce977d297f2abe560596b
Pavan Kalyan <125635201+pkalyan607@users.noreply.github.com>
2024-03-11 00:36:11 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -50,27 +50,84 @@ const Header = () => {
         <>
             <header className='header' id='header'>
                 {/* Mobile menu */}
-                <div id='menu' className='menu duration-300 bg-white lg:hidden shadow-lg w-[70%] h-[100%] fixed left-0 top-0 p-5'>
-                    <div className='text-3xl absolute right-3 cursor-pointer text-[#333]' onClick={() => closeMenu()} >
+                <div id='menu' className='menu flex-col overflow duration-200 bg-white lg:hidden shadow-lg w-[70%] h-[100%] fixed left-0 top-0 p-5 z-20'>
+                    <div className='text-2xl right-2 cursor-pointer text-[#222]' onClick={() => closeMenu()} >
                         <IoClose />
                     </div>
                     {/* Menu items */}
-                    <ul className=''>
-                        <li className='m-3 font-medium text-md'><a href="" rel="noopener noreferrer">For you</a></li>
-                        <li className='m-3 font-medium text-md'><a href="" rel="noopener noreferrer">Discover</a></li>
-                        <li className='m-3 font-medium text-md'><a href="" rel="noopener noreferrer">Hire</a></li>
-                        <li className='m-3 font-medium text-md'><a href="" rel="noopener noreferrer">Assest</a></li>
-                        <li className='m-3 font-medium text-md'><a href="" rel="noopener noreferrer">Jobs</a></li>
-                    </ul>
+                    <div className="aciton-area flex-col items-center my-2 text-center">
+                        <div className="login-btn rounded-full border my-2 px-4 py-1 border-[#dee8ff] ">
+                            <a href="" className='text-[#0057ff] font-bold text-md'>Log In</a>
+                        </div>
+                        <div></div>
+                        <div className="login-btn rounded-full border px-4 py-1 border-[#dee8ff] bg-[#0057ff] ">
+                            <a href="" className='text-white font-bold text-md'>Sign up</a>
+                        </div>
+                    </div>
+                    
+                    <div className="tools-item flex items-center border rounded-full px-2 py-2 mx-1 justify-between ">
+                            <div className="t-icon">
+                                            <MdOutlineSort />
+                            </div>
+                            <div className="tname px-2 ">
+                                <select id="" className='text-md font-bold cursor-pointer'> 
+                                            <option selected>Recommended</option>
+                                            {
+                                                dropdownData.map((result) => (
+                                                    <option value={result}>{result}</option>
+                                                    ))
+                                                }
+                                </select>
+                            </div>
+                            
+                    </div>
+                    
+                    <hr className='my-2'></hr>
+                    <div>
+                       
+                    {tagName.map((tools, index) => (
+                            <React.Fragment key={index}>
+                                <div className="tools-item flex items-center px-2 py-2 mx-1 my-2 justify-between">
+                                    <div className="t-icon">
+                                        {tools.tagsIcon}
+                                    </div>
+                                    <div className="tname px-2">
+                                        < p className='text-sm '>{tools.tags}</p>
+                                    </div>
+                                <div className="dt-arrow">
+                            <MdArrowDropDown />
+                    </div>
+                    </div>
+                            {index !== tagName.length - 1 && <hr />} {/* Add line break if not the last item */}
+                        </React.Fragment>
+                    ))}
+                </div>
+                <hr></hr>
+
+
+                    
+                    <div className='my-2'>
+                        <div></div>
+                        <ul className=''>
+                            <li className='m-2 font-medium text-md'><a href="" rel="noopener noreferrer">For you</a></li>
+                            <li className='m-2 font-medium text-md'><a href="" rel="noopener noreferrer">Discover</a></li>
+                            <li className='m-2 font-medium text-md'><a href="" rel="noopener noreferrer">Hire</a></li>
+                            <li className='m-2 font-medium text-md'><a href="" rel="noopener noreferrer">Assest</a></li>
+                            <li className='m-2 font-medium text-md'><a href="" rel="noopener noreferrer">Jobs</a></li>
+                        </ul>
+                    </div>
                 </div>
 
+                
+                
+
                 {/* Desktop header */}
-                <div className='border-b w-full top-0 bg-white'>
+                <div className='border-b w-full top-0 bg-white z-10'>
                     <div className="container-fluid">
-                        <div className="top-header py-4 lgpy-3 px-5 border-b border ">
+                        <div className="top-header py-4 lgpy-2 px-5 border-b border ">
                             <div className="navbar flex items-center justify-between">
                                 <div className="brand-logo flex items-center">
-                                    <div className="phone-menu pr-3 text-2xl cursor-pointer block lg:hidden" onClick={() => openMenu()}>
+                                    <div className="phone-menu pr-2 text-2xl cursor-pointer block lg:hidden" onClick={() => openMenu()}>
                                         <CgMenuLeft />
                                     </div>
                                     <img src={behance_logo} alt={behance_logo} className="w-auto h-4" />
@@ -78,11 +135,11 @@ const Header = () => {
                                     {/* Page links */}
                                     <div className="page-links  lg:ml-8 hidden lg:block">
                                         <ul className='flex items-center'>
-                                            <li className='mx-3 font-medium text-md active'><a href="" rel="noopener noreferrer">For you</a></li>
-                                            <li className='mx-3 font-medium text-md active'><a href="" rel="noopener noreferrer">Discover</a></li>
-                                            <li className='mx-3 font-medium text-md active'><a href="" rel="noopener noreferrer">Hire</a></li>
-                                            <li className='mx-3 font-medium text-md active'><a href="" rel="noopener noreferrer">Assest</a></li>
-                                            <li className='mx-3 font-medium text-md active'><a href="" rel="noopener noreferrer">Jobs</a></li>
+                                            <li className='mx-2 font-medium text-md active'><a href="" rel="noopener noreferrer">For you</a></li>
+                                            <li className='mx-2 font-medium text-md active'><a href="" rel="noopener noreferrer">Discover</a></li>
+                                            <li className='mx-2 font-medium text-md active'><a href="" rel="noopener noreferrer">Hire</a></li>
+                                            <li className='mx-2 font-medium text-md active'><a href="" rel="noopener noreferrer">Assest</a></li>
+                                            <li className='mx-2 font-medium text-md active'><a href="" rel="noopener noreferrer">Jobs</a></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -94,7 +151,7 @@ const Header = () => {
                                         <div className="login-btn mx-4 rounded-full border px-4 py-1 border-[#dee8ff] bg-[#0057ff]">
                                             <a href="" className='text-white font-medium text-md'>Sign Up</a>
                                         </div>
-                                        <span className='text-gray-300'>|</span>
+                                        <span className='text-gray-200'>|</span>
                                         <div className="adobe-btn mx-4 flex items-center hover:opacity-70">
 
                                             <img src={adobeLogo} alt={adobeLogo} className="w-5 h-5" />
@@ -102,15 +159,12 @@ const Header = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="phone-search block lg:hidden cursor-pointer pr-3 text-2xl">
-                                    <IoSearchSharp />
-                                </div>
                             </div>
                         </div>
 
                         {/* Behance tools */}
                         <div className="hidden lg:block">
-                            <div className="behance-toools flex items-center justify-evenly p-5 pb-3">
+                            <div className="behance-toools flex items-center justify-evenly p-5 pb-2">
                                 <div className='flex items-center'>
                                     {tagName.map((tools) => (
                                         <div className="tools-item flex items-center border rounded-md px-2 py-2 mx-1 justify-between">
@@ -154,28 +208,28 @@ const Header = () => {
                                             <IoSearchSharp />
                                         </div>
                                     </div>
-                                    <div className="tags-search bg-white px-4 py-3 border-l-2 ">
+                                    <div className="tags-search bg-white px-4 py-2 border-l-2 ">
                                         <ul className='flex items-center'>
-                                            <li className=' text-black lg:bg-black mx-1 py-1 lg:text-white px-3 rounded-full font-medium text-sm'><a href="">Projects</a></li>
+                                            <li className=' text-black lg:bg-black mx-1 py-1 lg:text-white px-2 rounded-full font-medium text-sm'><a href="">Projects</a></li>
                                             <div className="dt-arrow block lg:hidden">
                                                 <MdArrowDropDown />
                                             </div>
                                             {
                                                 searchType.map((tags) => (
-                                                    <li className='mx-1 font-medium text-sm py-1 px-3 hidden lg:block rounded-full hover:bg-[#eee]'><a href="">{tags.sItems}</a></li>
+                                                    <li className='mx-1 font-medium text-sm py-1 px-2 hidden lg:block rounded-full hover:bg-[#eee]'><a href="">{tags.sItems}</a></li>
                                                 ))
                                             }
                                         </ul>
                                     </div>
                                 </div>
                                 <div className='container absolute z-10'>
-                                    <ul style={{background:"white", borderBottomRightRadius:5, borderBottomLeftRadius:5, padding:10}}>
+                                    {filteredData.length != 0 && <ul style={{background:"white", borderBottomRightRadius:5, borderBottomLeftRadius:5, padding:10}}>
                                         {
                                             filteredData.map((result) => (
                                                 <li style={{padding:5, cursor:"pointer"}}>{result}</li>
                                             ))
                                         }
-                                    </ul>
+                                    </ul>}
                                 </div>
                             </div>
 
