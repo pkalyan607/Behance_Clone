7b7efe88e3208739795ee162aacaac4d9acc1ad0
Pavan Kalyan <125635201+pkalyan607@users.noreply.github.com>
2024-03-10 20:42:17 +0530

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,9 @@
 const reportWebVitals = onPerfEntry => {
+  // Check if onPerfEntry is a function
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // Dynamically import the web-vitals module
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      // Call each of the web-vitals methods with the onPerfEntry callback
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
